8a4dadc47f33c048113f0814e3f504b38605b202
leadpony <leadpony@openclosed.dev>
2025-02-22 18:50:33 +0900

    Remove manifest from main jar

--- hello-nonmodular/package-deb.sh
+++ hello-nonmodular/package-deb.sh
@@ -7,4 +7,5 @@ jpackage --name hello-nonmodular \
     --input target/jars \
     --dest target \
     --main-jar hello-nonmodular-1.0.0.jar \
+    --main-class org.example.DemoApplication \
     --verbose
