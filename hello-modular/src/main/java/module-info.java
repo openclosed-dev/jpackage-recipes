25814b0cb3f1edb37be5ea23ae12badd5a25f6d6
leadpony <leadpony@openclosed.dev>
2025-02-23 23:18:00 +0900

    Add an example using JPMS

---
+++ hello-modular/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+open module org.example.hello {
+    requires org.slf4j;
+    requires spring.beans;
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires spring.core;
+}
